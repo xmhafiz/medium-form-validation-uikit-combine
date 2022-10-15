5ff1380342ce0217cb7fab23e2f72c6aef989d2e
Mohd Hafiz <xmhafiz@gmail.com>
2022-10-15 21:13:59 +0800

    init proj

---
+++ TextFieldValidationApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TextFieldValidationApp
+//
+//  Created by Mohd Hafiz on 15/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
